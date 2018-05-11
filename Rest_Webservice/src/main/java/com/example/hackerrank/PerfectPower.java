4a46b6f859976ecc2f2319cbcbae6b2cf986c0c2
606905 <606905@LT036994.cts.com>
2018-05-11 17:19:23 +0530

    Hacker rank algorithm

---
+++ Rest_Webservice/src/main/java/com/example/hackerrank/PerfectPower.java
@@ -0,0 +1,30 @@
+package com.example.hackerrank;
+
+import java.util.Scanner;
+
+public class PerfectPower {
+	
+	public static void main(String[] args)
+	{
+		System.out.print("Enter Number");
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int x = Math.abs(input);
+        int y = x;
+        int p = 1;
+        for (int i = (int)Math.sqrt(x); i > 0; i--) {
+            for (int exp = 2; exp < 32; exp++) {
+                if ((int)Math.pow(i, exp) == x) {
+                    if (exp > p) {
+                        y = i;
+                        p = exp;
+                    }
+                    break;
+                } else if ((int)Math.pow(i, exp) >= x) {
+                    break;
+                }
+            }
+        }
+        System.out.println(p);
+	} 
+}
